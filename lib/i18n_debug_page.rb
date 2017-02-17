a3007a1f2e11a46292c7cf235f1f55d2707ea663
mike <yfractal@gmail.com>
2017-02-17 08:14:20 +0800

    collect i18n debug log for each action

--- lib/i18n_debug_page.rb
+++ lib/i18n_debug_page.rb
@@ -2,4 +2,17 @@ require "i18n_debug_page/engine"
 require "i18n_debug_page/monitor"
 
 module I18nDebugPage
+  def recollect_i18n_debug_logs
+    I18nDebugPage::Monitor.i18n_debug_logs = []
+  end
+
+  class << self
+    def included(base)
+      base.send(:before_action, :recollect_i18n_debug_logs)
+    end
+
+    def i18n_debug_logs
+      I18nDebugPage::Monitor.i18n_debug_logs
+    end
+  end
 end
--- lib/i18n_debug_page/monitor.rb
+++ lib/i18n_debug_page/monitor.rb
@@ -2,11 +2,14 @@ require 'i18n/debug'
 module I18nDebugPage
   class Monitor
     class << self
-      attr_accessor :i18n_debug_infos
+      attr_accessor :i18n_debug_logs
 
       I18n::Debug.on_lookup do |key, result|
-        Monitor.i18n_debug_infos ||= []
-        Monitor.i18n_debug_infos << { key: key, result: result }
+        Monitor.i18n_debug_logs << { key: key, result: result }
+      end
+
+      def i18n_debug_logs
+        @i18n_debug_logs ||= []
       end
     end
   end
